c46d396b4872b08d0375ad983bf96243cb897581
hk_19 <himanshu.khatri@dianapps.com>
2025-03-06 15:39:29 +0530

    project setup

---
+++ member/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    booking_count = models.IntegerField(default=0)
+    date_joined = models.DateTimeField(auto_now_add=True)
